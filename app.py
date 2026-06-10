6bfd07475b6f4b2854ee36ca1aead851e12fbef4
Aatenasharifi <a.atenasharifi2009@gmail.com>
2026-06-10 09:48:00 -0700

    admin blueprint added

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from blueprins.general import app as general
+from blueprins.admin import app as admin
 
 app = Flask(__name__)
 
 app.register_blueprint(general)
+app.register_blueprint(admin)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
