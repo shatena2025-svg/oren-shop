70f4fc2c8786d25735edcc0053bbf381e47d1e7a
Aatenasharifi <a.atenasharifi2009@gmail.com>
2026-06-11 22:15:55 -0700

    product page added

--- blueprins/general.py
+++ blueprins/general.py
@@ -5,8 +5,14 @@ app = Blueprint('general',__name__)
 
 @app.route('/')
 def main(): 
-    products = Product.query.all()
+    products = Product.query.filter(Product.active == 1).all()
     return render_template('main.html',products=products)
+
+@app.route('/product/<int:id>/<name>')
+def product(id , name):
+    product = Product.query.filter(Product.id == id).filter(Product.active == 1).filter(Product.name == name).first_or_404()
+    return render_template('product.html',product=product)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
