eeeff890a3181390b9761cd4b1211f38c9f1a0a5
Aatenasharifi <a.atenasharifi2009@gmail.com>
2026-06-11 20:55:32 -0700

    main and about added

--- blueprins/general.py
+++ blueprins/general.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
+from models.product import Product
 
 app = Blueprint('general',__name__)
 
 @app.route('/')
-def main():
-    return 'main page'
-
+def main(): 
+    products = Product.query.all()
+    return render_template('main.html',products=products)
 @app.route('/about')
 def about():
-    return 'about us'
+    return render_template('about.html')
